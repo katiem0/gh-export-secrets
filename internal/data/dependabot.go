20c1499021929a8adf3ab928a858f19a0708a4ce
Katie May <100700631+katiem0@users.noreply.github.com>
2023-04-04 15:09:20 -0400

    Linting error fixes

--- internal/data/dependabot.go
+++ internal/data/dependabot.go
@@ -24,7 +24,9 @@ func (g *APIGetter) GetRepoDependabotSecrets(owner string, repo string) ([]byte,
 	url := fmt.Sprintf("repos/%s/%s/dependabot/secrets", owner, repo)
 
 	resp, err := g.restClient.Request("GET", url, nil)
-
+	if err != nil {
+		log.Fatal(err)
+	}
 	responseData, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
